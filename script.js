6d2c42305a9a558349f1c00944ddfaaa98e0adff
pwongyu <114160274+pwongyu@users.noreply.github.com>
2022-12-13 17:12:00 +0900

    221213 수정중

--- script.js
+++ script.js
@@ -3,12 +3,3 @@
 // document.querySelector
 // document.querySelectorAll
 // addEventListener
-
-const=navMainMenuLi=document.getElementsByClassName("nav-m-m-li")
-const=navMainSubbox=document.getElementsByClassName("nav-m-subbox")
-
-
-
-navMainMenuLi.addEventListener('onmouseover', (event) => {
-    navMainSubbox.style.display="block";
-})
