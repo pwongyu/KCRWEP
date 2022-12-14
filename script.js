1f2d3e0fd5c864bc1e56ebb99bfe087db783332f
pwongyu <114160274+pwongyu@users.noreply.github.com>
2022-12-14 23:13:46 +0900

    221214홈페이지 수정중 슬라이드까진 완성

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 // document.querySelector
 // document.querySelectorAll
 // addEventListener
+
+
+// 이미지 슬라이드 페이드인 페이드아웃
+$('#slide > div:gt(0)').hide();
+
+setInterval(function(){
+    $('#slide > div:first')
+        .fadeOut(1500)
+        .next()
+        .fadeIn(1500)
+        .end()
+        .appendTo('#slide');
+},7000);
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
